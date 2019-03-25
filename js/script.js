2fa39b57073b261f3e5355e3748177cd441fedce
Betsabe Bajana <betsabe525@gmail.com>
2019-03-25 22:14:27 +0000

    first draft

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@
 /* global $ */
 
 $("#search-button").click(function(){
+  var user_input = $("#search-term").val();
   
-  
-  
+  $.ajax({
+      url: "https://api.giphy.com/v1/gifs/search?q=" + user_input + "&rating=pg&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+          $("#images").empty();
+          $("#images").append("<img src='" + response.data[0].images.original.url + "'>");
+      }
+  });
 });
 
