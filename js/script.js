9fc2f13672f2a5796ce07b92b352c48c6b3b70c5
Betsabe Bajana <betsabe525@gmail.com>
2019-04-01 22:09:03 +0000

    Final Draft

--- js/script.js
+++ js/script.js
@@ -5,13 +5,28 @@
 
 $("#search-button").click(function(){
   var user_input = $("#search-term").val();
+  var rand_num = Math.floor(Math.random() * 20);
   
   $.ajax({
       url: "https://api.giphy.com/v1/gifs/search?q=" + user_input + "&rating=pg&api_key=dc6zaTOxFJmzC",
       method: "GET",
       success: function(response) {
           $("#images").empty();
-          $("#images").append("<img src='" + response.data[0].images.original.url + "'>");
+          $("#images").append("<img src='" + response.data[rand_num].images.original.url + "'>");
+      }
+  });
+});
+
+$("#rand-bun").click(function(){
+  var rand_num1 = Math.floor(Math.random() * 20);
+  var rand_num2 = Math.floor(Math.random() * 1000);
+  
+  $.ajax({
+      url: "https://api.giphy.com/v1/gifs/search?q=" + rand_num2 + "&rating=pg&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+          $("#images").empty();
+          $("#images").append("<img src='" + response.data[rand_num1].images.original.url + "'>");
       }
   });
 });
